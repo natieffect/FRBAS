43f82e6cc87fbdec6626c9c8a8372d404d0f57a5
natieffect <natnaelmasresha2022@gmail.com>
2023-06-10 17:30:53 +0300

    employee models updated to include account,department,job,work tables

--- FRBAS/employee/models.py
+++ FRBAS/employee/models.py
@@ -41,7 +41,7 @@ class Employee(Detail,Status):
      fname     = models.CharField("Full Name ",max_length=100, help_text="employee full name")
      phone     = models.CharField("Phone Number", max_length=10, help_text="employee phone number", unique=True)
      birth     = models.DateField("Date Of Birth", help_text="employee date of birth")
-     id        = models.CharField("ID", null=True, blank=True, help_text="employee compony id")
+     id        = models.CharField("ID", null=True,max_length=100,  blank=True, help_text="employee compony id")
      gender    = models.CharField("Gender",choices=GENDER, max_length=2, help_text="employee gender")
      coutry    = models.CharField("Country",choices=CITY, max_length=3, help_text="employee currently living country")
      
@@ -57,30 +57,34 @@ class Account(Detail,Status):
           return self.user.fname
 
 class Department(Detail,Status):
-     name   = models.CharField("Name",max_length=50,help_text="department name")
-     detail = models.CharField("Description",blank=True,null=True,help_text="department description")
-     id     = models.CharField("ID",blank=True,null=True,help_text="department id")
+     name    = models.CharField("Name",max_length=50,help_text="department name")
+     detail  = models.CharField("Description",max_length=100,blank=True,null=True,help_text="department description")
+     depid   = models.CharField("ID",max_length=100,blank=True,null=True,help_text="department id")
+     creater = models.ForeignKey(Employee, on_delete=models.CASCADE, help_text="employee with autority create the department") 
      class Meta:
-         unique_together = ('name', 'id')
+         unique_together = ('name', 'depid')
 
      def __str__(self) -> str:
           return self.name
 
 class Job(Detail,Status):
-     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+     department = models.ForeignKey(Department, on_delete=models.CASCADE,help_text="job created as which part of department")
      title      = models.CharField("Title",max_length=50,help_text="job title")
-     detail     = models.CharField("Description",blank=True,null=True,help_text="job description")
-     id         = models.CharField("ID",blank=True,null=True,help_text="job id")
+     detail     = models.CharField("Description",max_length=100,blank=True,null=True,help_text="job description")
+     jobid      = models.CharField("ID",max_length=100,blank=True,null=True,help_text="job uniqe identifying id")
+     creater    = models.ForeignKey(Employee, on_delete=models.CASCADE, help_text="employee with autority create the job") 
      class Meta:
-         unique_together = ('department','title', 'id')
+         unique_together = ('department','title', 'jobid')
 
      def __str__(self) -> str:
           return self.title
 
 class Work(Detail):
      employee  = models.OneToOneField(Employee, on_delete=models.CASCADE,help_text="employee detail")
+     job       = models.ForeignKey(Job,on_delete=models.CASCADE,help_text=" employee job description")
      begin     = models.TimeField("Work Start",default=WORK_START,help_text="employee work start")
      finish    = models.TimeField("Work End",default=WORK_START,help_text="employee work end")
+     assigned  = models.ForeignKey(Employee,related_name='assigned', on_delete=models.CASCADE, help_text="employee assigned by which employee autorizesd")
 
      def __str__(self) -> str:
-          return self.employee.fname
+          return f"{self.employee.fname} works {self.job.title}"

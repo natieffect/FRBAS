7b4cb3bcbccd023c214b21b0ea746d1ca778108b
natieffect <natnaelmasresha2022@gmail.com>
2023-06-10 17:01:57 +0300

    employee based model class, database tables , properties generated

--- FRBAS/employee/models.py
+++ FRBAS/employee/models.py
@@ -1,3 +1,86 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+GENDER = [
+     ('m',"Male"),
+     ('f',"Female")
+]
+
+CITY =[  
+         ("aa", "Addis Ababa"),
+         ("naz","Nazrēt"),
+         ("god", "Gonder"),
+         ("mek","Mekele"),
+         ("aws","Āwasa"),
+         ("dd","Dire Dawa"),
+         ("bah","Bahir Dar"),
+         ("har", "Harar"),
+         ("jig", "Jijiga"),
+         ("aso", "Āsosa"),
+         ("gam", "Gambēla"),
+         ("sem", "Semera")
+    ]
+
+SYSTEM_AUTHORITY = [('ex','Expert'),('ad','Admin'),('ep','Employee')]
+class Detail(models.Model):
+     pub_date = models.DateTimeField(auto_now_add=True ,null=True,help_text="published date")
+     class Meta:
+         abstract = True
+
+WORK_START  = datetime.time(2,30,0)
+WORK_END    = datetime.time(10,59,0)
+
+class Status(models.Model):
+     active = models.PositiveSmallIntegerField(default=1,max_length=2, help_text="active or inactive status")
+     trash  = models.PositiveSmallIntegerField(default=0,max_length=2, help_text="deleted or not deleted status")
+     class Meta:
+         abstract = True
+         
+class Employee(Detail,Status):
+     email     = models.EmailField("Email",unique=True,primary_key=True,help_text="employee unique email account")
+     fname     = models.CharField("Full Name ",max_length=100, help_text="employee full name")
+     phone     = models.CharField("Phone Number", max_length=10, help_text="employee phone number", unique=True)
+     birth     = models.DateField("Date Of Birth", help_text="employee date of birth")
+     id        = models.CharField("ID", null=True, blank=True, help_text="employee compony id")
+     gender    = models.CharField("Gender",choices=GENDER, max_length=2, help_text="employee gender")
+     coutry    = models.CharField("Country",choices=CITY, max_length=3, help_text="employee currently living country")
+     
+     def __str__(self) -> str:
+          return self.fname
+
+class Account(Detail,Status):
+     user     = models.OneToOneField(Employee, on_delete=models.CASCADE,help_text="employee")
+     password = models.CharField("Password", max_length=100, help_text="User system access password")
+     autority = models.CharField("System Autority",max_length=2, choices=SYSTEM_AUTHORITY, default='ep', help_text="user privilages over system")
+
+     def __str__(self) -> str:
+          return self.user.fname
+
+class Department(Detail,Status):
+     name   = models.CharField("Name",max_length=50,help_text="department name")
+     detail = models.CharField("Description",blank=True,null=True,help_text="department description")
+     id     = models.CharField("ID",blank=True,null=True,help_text="department id")
+     class Meta:
+         unique_together = ('name', 'id')
+
+     def __str__(self) -> str:
+          return self.name
+
+class Job(Detail,Status):
+     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+     title      = models.CharField("Title",max_length=50,help_text="job title")
+     detail     = models.CharField("Description",blank=True,null=True,help_text="job description")
+     id         = models.CharField("ID",blank=True,null=True,help_text="job id")
+     class Meta:
+         unique_together = ('department','title', 'id')
+
+     def __str__(self) -> str:
+          return self.title
+
+class Work(Detail):
+     employee  = models.OneToOneField(Employee, on_delete=models.CASCADE,help_text="employee detail")
+     begin     = models.TimeField("Work Start",default=WORK_START,help_text="employee work start")
+     finish    = models.TimeField("Work End",default=WORK_START,help_text="employee work end")
+
+     def __str__(self) -> str:
+          return self.employee.fname

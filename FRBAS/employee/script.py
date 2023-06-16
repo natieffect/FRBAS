8b6220155dbaaa9c32218affb0a401463d2fba32
natieffect <natnaelmasresha2022@gmail.com>
2023-06-16 09:16:10 +0300

    updated employee signup to create default employee acount and added employee password change

--- FRBAS/employee/script.py
+++ FRBAS/employee/script.py
@@ -1,4 +1,4 @@
-from .models import Employee,GENDER,CITY
+from .models import Employee,GENDER,CITY,SYSTEM_AUTHORITY,Account
 from django.contrib.auth.hashers import make_password, check_password
 from frbasScript import  FrbasScript
 
@@ -6,17 +6,19 @@ from frbasScript import  FrbasScript
 class EmployeeScript:
     def __init__(self) -> None:
          self.mainscript = FrbasScript()
-    
+#     Employee Sign Up 
     def employeeSignup(self,request):
         try:
              employee = Employee(
-                                             email=request.POST.get("email"), 
-                                             fname = request.POST.get("fname"),
-                                             phone = request.POST.get("phone"),
-                                             birth = request.POST.get("birth"),
-                                             id = request.POST.get("id"),
-                                             gender = request.POST.get("gender"),
-                                             coutry = request.POST.get("country"))
+                                   email=request.POST.get("email"), 
+                                   fname = request.POST.get("fname"),
+                                   phone = request.POST.get("phone"),
+                                   birth = request.POST.get("birth"),
+                                   id = request.POST.get("id"),
+                                   gender = request.POST.get("gender"),
+                                   coutry = request.POST.get("country"))
+             
+             account = Account(user=employee,password=self.mainscript.config.get("DEFAULT","PASSWORD"),autority="")
              
              request = self.mainscript.set_message("success","EMPLOYEE_SIGN_UP","SIGNUP_SUCCESS",request)
         except Exception as e: 
@@ -24,18 +26,36 @@ class EmployeeScript:
              request = self.mainscript.set_message("error","EMPLOYEE_SIGN_UP","SIGNUP_ERROR",request)
         return request
    
-#     Employee Gender Display
+#     Employee Gender 
     def employeeGender(self):
          gender = []
          for value in GENDER:
               gender.append({'value': value[0], 'name':value[1]})
          return gender
     
-#     Employee Country Display 
+#     Employee CITY  
     def employeeCountry(self):
          country = []
          for value in CITY:
               country.append({'value': value[0], 'name':value[1]})
          return country
-         
-         
+
+#     Employee SYSTEM_AUTHORITY  
+    def employeeAuthority(self):
+         autority = []
+         for value in SYSTEM_AUTHORITY:
+              autority.append({'value': value[0], 'name':value[1]})
+         return autority
+
+#    Employee Password Change
+    def employeePassword(self,request):
+         try:
+              employee = Employee.object.get(email=request.session.get('account_email',False))
+              if request.POST['password'] == request.POST['conformpassword']:
+                    employee.password = make_password(request.POST['password'])
+                    request = self.mainscript.set_message("success","EMPLOYEE_ACCOUNT","ACCOUNT_PASSWORD_SUCCESS",request)
+              else:
+                    request = self.mainscript.set_message("error","EMPLOYEE_ACCOUNT","ACCOUNT_PASSWORD_ERROR_ONE",request)
+         except Exception as e:
+               print(e)
+               request = self.mainscript.set_message("error","EMPLOYEE_ACCOUNT","ACCOUNT_PASSWORD_ERROR_TWO",request)
--- FRBAS/employee/views.py
+++ FRBAS/employee/views.py
@@ -5,6 +5,12 @@ def employeeSignUp(request):
      employee = EmployeeScript()
      gender = employee.employeeGender()
      country = employee.employeeCountry()
+     autority = employee.employeeAuthority()
      if request.method == "POST":
          request = employee.employeeSignup(request)
-     return render(request,"employee/employeeSignup.html",{"gender":gender,"country":country})
+     return render(request,"employee/employeeSignup.html",{"gender":gender,"country":country,"autority":autority})
+
+# Employee Account Password Change
+def employeeAccountPassword(request):
+     employee = EmployeeScript()
+     pass

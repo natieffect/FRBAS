4166887d50e35a49fa10c6ef2783353196341b96
natieffect <natnaelmasresha2022@gmail.com>
2023-06-16 08:24:38 +0300

    added employee app view to handle request on user sign up

--- FRBAS/employee/views.py
+++ FRBAS/employee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .script import  EmployeeScript
 # Create your views here.
+def employeeSignUp(request):
+     employee = EmployeeScript()
+     gender = employee.employeeGender()
+     country = employee.employeeCountry()
+     if request.method == "POST":
+         request = employee.employeeSignup(request)
+     return render(request,"employee/employeeSignup.html",{"gender":gender,"country":country})

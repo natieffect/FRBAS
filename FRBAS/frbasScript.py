bdd5586c6fe6551f1040d1b27b99f909e2058bbe
natieffect <natnaelmasresha2022@gmail.com>
2023-06-16 10:26:53 +0300

    employee login sesstion generator added to frbasScript.py script

--- FRBAS/frbasScript.py
+++ FRBAS/frbasScript.py
@@ -8,7 +8,7 @@ class FrbasScript:
       self.config = ConfigParser()
       self.config.read(f'{os.path.join(Path(__file__).resolve().parent,"configration")}/frbas_text_config.cfg')
          
-   # set message to request value
+#   set message to request value
     def set_message(self, type,catagory, msg, request):
         if type == 'success':
              messages.add_message(request, messages.SUCCESS, self.config.get(catagory, msg))
@@ -21,7 +21,20 @@ class FrbasScript:
         
         return request
 
-    # Create your views here.
+#    Employee Log in sesstion create
+    def login_sesstion(self,account,request):
+         success = False
+         try:
+              request.session['user_email'] = account.user.email
+              request.session['user_autority'] = account.autority
+              request = self.set_message("success","EMPLOYEE_ACCOUNT","ACCOUNT_LOGIN_SUCCESS",request)
+              success = True
+         except Exception as e:
+              print(e)
+              request = self.set_message("error","DEFAULT","SESSTION_ERROR",request)
+         return request ,success
+    
+#    Create your views here.
     def log_out(self,request):
         for key in list(request.session.keys()):
              del request.session[key]
